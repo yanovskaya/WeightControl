a57c5995614b9c36bf896786ffc10f9223277394
yanovskaya <yanovskaya1997@list.ru>
2018-05-26 12:41:09 +0300

    создание базового ui

---
+++ WeightControl/CollectionViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CollectionViewController.swift
+//  WeightControl
+//
+//  Created by Елена Яновская on 26.05.2018.
+//  Copyright © 2018 Elena Yanovskaya. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+}
---
+++ WeightControl/WeightCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeightCell.swift
+//  WeightControl
+//
+//  Created by Елена Яновская on 26.05.2018.
+//  Copyright © 2018 Elena Yanovskaya. All rights reserved.
+//
+
+import UIKit
+
+class WeightCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
